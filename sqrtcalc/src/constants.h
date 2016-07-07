b412b2bac341fd3b76b0d58bba99b09f2c567fbd
gorostuck <joelperezferrer@gmail.com>
2016-07-07 20:54:19 +0200

    Started sqrtcalc

---
+++ sqrtcalc/src/constants.h
@@ -0,0 +1,8 @@
+#include <string>
+
+const string SUCCESS = "Good job!";
+const string FAIL    = "You failed!";
+const string ERR     = "Enter a natural number!";
+const string ENUN    = "Aprox the sqrt of: ";
+const strign START   = "This game consists in aproximating the square root of a number, enter 0 to exit.";
+const string EXIT    = "You scored: ";
---
+++ sqrtcalc/src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "constants.h"
+
+int main ()
+{
+    printf(START);
+    unsigned int input = 1;
+    while (input!=0){
+        unsigned int sqrt_of = 0; 
+    }
+}
