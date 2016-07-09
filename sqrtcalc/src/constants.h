cd582a84f2c973b537278cbba92d106a1e10c8b7
gorostuck <joelperezferrer@gmail.com>
2016-07-09 15:40:15 +0200

    Segmentation fault tho

--- sqrtcalc/src/constants.h
+++ sqrtcalc/src/constants.h
@@ -1,8 +1,9 @@
-#include <string>
+// This file contains constants, nothing interesting
+#include <string.h>
 
-const string SUCCESS = "Good job!";
-const string FAIL    = "You failed!";
-const string ERR     = "Enter a natural number!";
-const string ENUN    = "Aprox the sqrt of: ";
-const strign START   = "This game consists in aproximating the square root of a number, enter 0 to exit.";
-const string EXIT    = "You scored: ";
+const char* SUCCESS = "Good job!";
+const char* FAIL    = "You failed!";
+const char* ERR     = "Enter a natural number!";
+const char* ENUN    = "Aprox the sqrt of: ";
+const char* START   = "This game consists in aproximating the square root of a number, enter 0 to exit.";
+const char* EXIT    = "You scored: ";
--- sqrtcalc/src/main.c
+++ sqrtcalc/src/main.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "constants.h"
 
 int main ()
 {
-    printf(START);
-    unsigned int input = 1;
+    printf("%s\n", START);
+    int input = 1;
+    unsigned int score = 0;
     while (input!=0){
-        unsigned int sqrt_of = 0; 
+        unsigned int sqrt_of   = rand() % 100;
+        printf("%s%d\n", ENUN, sqrt_of);
+        if (scanf("%d", input)==0)
+            printf("%s\n",ERR);
+        else {
+            double sqrt_a = sqrt(sqrt_of);
+            if (input != sqrt_a)
+                printf("%s\n", FAIL);
+            else { printf("%s\n", SUCCESS); ++score; }
+        }
     }
+    printf("%s%d\n", EXIT, score);
+    return 0;
 }
