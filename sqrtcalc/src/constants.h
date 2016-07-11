de5666db4540f0115b95b447afdb8d399df3e9f9
gorostuck <joelperezferrer@gmail.com>
2016-07-11 21:01:43 +0200

    Finished

--- sqrtcalc/src/constants.h
+++ sqrtcalc/src/constants.h
@@ -7,3 +7,5 @@ const char* ERR     = "Enter a natural number!";
 const char* ENUN    = "Aprox the sqrt of: ";
 const char* START   = "This game consists in aproximating the square root of a number, enter 0 to exit.";
 const char* EXIT    = "You scored: ";
+const char* ANSW    = "Answer was: ";
+const char* MAX_Q   = "What range do you want? 0-X";
--- sqrtcalc/src/main.c
+++ sqrtcalc/src/main.c
@@ -5,19 +5,24 @@
 
 int main ()
 {
-    printf("%s\n", START);
     int input = 1;
     unsigned int score = 0;
-    while (input!=0){
-        unsigned int sqrt_of   = rand() % 100;
+    unsigned int max_range = 1;
+    printf("%s\n%s", START, MAX_Q);
+    while (scanf ("%u", &max_range)!=0);
+    while (1){
+        unsigned int sqrt_of   = rand() % max_range;
         printf("%s%d\n", ENUN, sqrt_of);
-        if (scanf("%d", input)==0)
-            printf("%s\n",ERR);
-        else {
-            double sqrt_a = sqrt(sqrt_of);
-            if (input != sqrt_a)
-                printf("%s\n", FAIL);
-            else { printf("%s\n", SUCCESS); ++score; }
+        scanf("%d", &input);
+        int sqrt_a = sqrt(sqrt_of);
+        double sqrt_full = sqrt(sqrt_of); 
+        if (input == 0) break;
+        if (input != sqrt_a) {
+            printf("%s\n", FAIL);
+            printf("%s%d(%f)\n", ANSW, sqrt_a, sqrt_full);
+        } else { 
+            printf("%s\n", SUCCESS);
+            ++score; 
         }
     }
     printf("%s%d\n", EXIT, score);
