dd9a36ebcbd63bde9d77377fd2a0702ed643d548
gorostuck <joelperezferrer@gmail.com>
2016-07-12 17:13:27 +0200

    Now it's nice!

--- ncursor/src/main.c
+++ ncursor/src/main.c
@@ -1,4 +1,5 @@
 #include <ncurses.h>
+#include "window.h"
 
 WINDOW *create_newwin(int height, int width, int starty, int startx);
 void destroy_win(WINDOW *local_win);
@@ -23,20 +24,3 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-WINDOW *create_newwin(int height, int width, int starty, int startx)
-{   WINDOW *local_win;
-    
-    local_win = newwin(height, width, starty, startx);
-    box(local_win, 0, 0);
-
-    wrefresh(local_win);
-
-    return local_win;
-}
-
-void destroy_win(WINDOW *local_win)
-{
-    wborder(local_win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
-    wrefresh(local_win);
-    delwin(local_win);
-}
