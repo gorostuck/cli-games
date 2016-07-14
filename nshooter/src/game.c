9537e889fab3f0298ba2e988979e735af48e5137
gorostuck <joelperezferrer@gmail.com>
2016-07-14 20:13:18 +0200

    Updated

--- nshooter/src/game.c
+++ nshooter/src/game.c
@@ -13,27 +13,69 @@ int game_start()
     move(0,0);
     getch();
 
+    main_window = window_in_middle(MAX_Y, MAX_X, ROWS, COLS);
+    move(ROWS/2, COLS/2);
+
     while (game_loop());
 
+    destroy_win(main_window);
+
     endwin();
+
     return 0;
 }
 
 int game_loop()
 {
-    WINDOW *main_window = window_in_middle(20, 45, ROWS, COLS);
-    print_in_middle(ROWS, COLS, S_GAME_START);
-    move(0,0);
-    getch();
-    destroy_win(main_window);
-    return 0;
+    getyx(main_window, current_y, current_x);
+    while(move_within_border());
+    refresh();
+    return 1;
+}
+
+int move_within_border()
+{
+    int ch = getch();
+    switch(ch)
+    {
+        case KEY_UP:
+            printw("%d", current_y);
+            if (current_y < MAX_Y){
+                move(++current_y, current_x);
+                return 0;
+            }
+            break;
+        case KEY_LEFT:
+            if (current_x > 0){
+                move(current_y, ++current_x);
+                return 0;
+            }
+            break;
+        case KEY_DOWN:
+            if (current_x < MAX_X){
+                move(--current_y, current_x);
+                return 0;
+            }
+            break;
+        case KEY_RIGHT:
+            if (current_y > 0){
+                move(current_y, current_x++);
+            }
+        default:
+            break;
+    }
+    return 1;
 }
 
 void init()
 {
     load_constants();
     initscr();
+    cbreak();
+    noecho();
     getmaxyx(stdscr, ROWS, COLS);
+
+    box(stdscr, 0,0);
 }
 
 void load_constants()
--- nshooter/src/game.h
+++ nshooter/src/game.h
@@ -1,14 +1,23 @@
 #ifndef GAME_H_INCLUDED
 #define GAME_H_INCLUDED
 
+#include <ncurses.h>
+
+#define MAX_Y 20
+#define MAX_X 45
+
+WINDOW *main_window;
+
+int ROWS, COLS;
+
+int current_y, current_x;
+
 char* S_WELCOME;
 char* S_GAME_START;
 
-int ROWS;
-int COLS;
-
 int game_start();
 int game_loop();
+int move_within_border();
 void init();
 void load_constants();
 
