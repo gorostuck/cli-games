e5892c14784f48c30e307321397ea3369ad5c2ff
Joel Perez <joelperezferrer@gmail.com>
2016-07-27 19:26:22 +0200

    And now I've added the file itself

---
+++ nshooter/src/constants.h
@@ -0,0 +1,34 @@
+// This file contains constants for several purposes
+#ifndef CONSTANTS_H_DEFINED
+#define CONSTANTS_H_DEFINED
+
+/* MACROS */
+//#define DEBUG_MODE
+
+/* DIRECTIONS */
+#define NONE  0
+#define LEFT  1
+#define RIGHT 2
+#define DOWN  3
+#define UP    4
+
+/* KEYS */
+#define KEY_SPACE (int)' '
+
+#define KEY_K     (int)'k'
+#define KEY_J     (int)'j'
+#define KEY_H     (int)'h'
+#define KEY_L     (int)'l'
+
+/* MAIN_SCREEN */
+#define MAIN_SCREEN_ROWS 20
+#define MAIN_SCREEN_COLS 45
+
+/* AMMO */
+#define AMMO_SCREEN_ROWS 4
+#define AMMO_SCREEN_COLS 30
+
+#define MAX_AMMO AMMO_SCREEN_COLS/2
+
+
+#endif //CONSTANTS_H_DEFINED
