6eb3b536824b0a44330821aafb8ce66d53f2df55
Joel Perez <joelperezferrer@gmail.com>
2016-08-02 10:14:14 +0200

    I always forget to track new files lol

---
+++ nshooter/src/tutorial_screen.c
@@ -0,0 +1,2 @@
+// This file contains the definitions for stuff related to the tutorial screen
+#include tutorial_screen.h
---
+++ nshooter/src/tutorial_screen.h
@@ -0,0 +1,9 @@
+// This file contains the declarations for stuff related to the tutorial screen
+#ifndef TUTORIAL_SCREEN_H_DEFINED
+#define TUTORIAL_SCREEN_H_DEFINED
+
+#include "window.h"
+WINDOW* tutorial_screen;
+
+print_in_tutorial(char* str);
+#enfid /* TUTORIAL_SCREEN_H_DEFINED */
